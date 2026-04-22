003c3cbd852351722868c5c1ec63b36efaf4cfee
Alex Gavrilov <alex@zello.com>
2026-04-21 19:43:06 -0500

    Version 0.4.1

--- client-ios/SerenadaCore/Sources/SerenadaCore.swift
+++ client-ios/SerenadaCore/Sources/SerenadaCore.swift
@@ -38,7 +38,7 @@ public struct CreateRoomResult {
 @MainActor
 public final class SerenadaCore {
     /// SDK version string.
-    public static let version = "0.4.0"
+    public static let version = "0.4.1"
 
     /// SDK configuration.
     public let config: SerenadaConfig
